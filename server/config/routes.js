1fe0873585c23a99387433f36644ebb6615441cf
Laura <lew4f08@gmail.com>
2016-07-05 16:55:46 -0700

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,15 @@
+
+var users = require('../controllers/INSERT_NAME_HERE.js');
+
+module.exports = function(app){
+
+  //user queries
+  app.get('#', function(req, res){
+
+  })
+
+  app.post('#', function(req,res){
+
+  })
+
+}
