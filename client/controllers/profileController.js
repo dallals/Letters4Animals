e9917a17d8071d8e5a55c497f94d41d131ebaec8
Sharol Chand <sharolchand@gmail.com>
2016-07-08 12:41:46 -0700

    user profile show/hide password and style changes

--- client/controllers/profileController.js
+++ client/controllers/profileController.js
@@ -1,4 +1,28 @@
 AnimalApp.controller('profileController', function ($scope, $routeParams, $http) {
     // Get current user from db
-    // 
+    // using dummy static data for now
+    $scope.user = {
+        first_name : 'John',
+        last_name : 'Smith',
+        street_address: '123 Street Way',
+        city: 'City',
+        state: 'CA',
+        zipcode : '35522',
+        phone_number: '4083453455',
+        email: 'test@user.com',
+        password: 'testpassword',
+        volunteer: true
+    }
+
+    // The following is if the user wants to update their password, they can show/hide their password
+    $scope.showpass = 'show';
+    $scope.showPassword = function(){
+        if($scope.showpass == 'hide'){
+            $scope.showpass = 'show';
+            $('#password').attr('type', 'password');
+        }else{
+            $scope.showpass = 'hide';
+            $('#password').attr('type', 'text');
+        }
+    }
 });
