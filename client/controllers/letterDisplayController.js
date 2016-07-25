d0d5dbc8749af32488264d4943dfde2d4b2adeca
Sharol Chand <sharolchand@gmail.com>
2016-07-25 15:31:56 -0700

    Added letter guest address info, and style

--- client/controllers/letterDisplayController.js
+++ client/controllers/letterDisplayController.js
@@ -11,21 +11,28 @@ AnimalApp.controller('letterDisplayController', function ($scope, $location, $ro
     $scope.logoDown     = false;
     $scope.supported    = false;
     $scope.fixed        = {
-                name    : '',
-                pos     : '',
-                addr    : '',
-                city    : '',
-                state   : '',
-                zip     : '',
-                pic     : ''
+        name    : '',
+        pos     : '',
+        addr    : '',
+        city    : '',
+        state   : '',
+        zip     : '',
+        pic     : ''
     }
     $scope.showDetails = false;
+    $scope.showGuestFields = false;
+
+    // Steps for Guest-Users
+    $scope.steps = ['Select Cause', 'Enter Guest Inforamtion', 'View Representatives', 'Preview Letter', 'Print/Save'];
 
     UserFactory.isLoggedIn(function(user){
         if(user.id){
             // If logged in, populate form with user info
             $scope.loggedUser = user;
             $scope.loggedIn = true;
+            // Steps for Logged In users
+            $scope.steps = ['Select Cause', 'View Representatives', 'Preview Letter', 'Print/Save'];
+          //
         }
     });
 
@@ -49,7 +56,7 @@ AnimalApp.controller('letterDisplayController', function ($scope, $location, $ro
         }
         else{
             var payload             = {};
-                payload.rep_level   = level;
+            payload.rep_level   = level;
 
             // Format address to send to civics API
             if($scope.loggedIn){
@@ -111,15 +118,15 @@ AnimalApp.controller('letterDisplayController', function ($scope, $location, $ro
 
                     // Format the address to upper-case for letter
                     var formAdd   = rep.rep.address[0].line1.split(' ').map(function(word){
-                                        var upWord = word.charAt(0).toUpperCase() + word.slice(1);
-                                        return upWord;
-                                    }).join(' ');
+                        var upWord = word.charAt(0).toUpperCase() + word.slice(1);
+                        return upWord;
+                    }).join(' ');
 
                     // .map(function(word){ word = word[0].toUpperCase() + word.slice(1)}).join(' ');
                     var formCity  = rep.rep.address[0].city.split(' ').map(function(word){
-                                        var upWord = word.charAt(0).toUpperCase() + word.slice(1);
-                                        return upWord;
-                                    }).join(' ');
+                        var upWord = word.charAt(0).toUpperCase() + word.slice(1);
+                        return upWord;
+                    }).join(' ');
 
                     rep.rep.address[0].line1 = formAdd;
                     rep.rep.address[0].city = formCity;
@@ -183,10 +190,10 @@ AnimalApp.controller('letterDisplayController', function ($scope, $location, $ro
             letter.children[0].style = 'float: right; margin-right: 45px; margin-top: 25px';
 
             var letterName  = 'Letter_to_' + letter.children[11].innerHTML + '.doc',
-                letterName  = letterName.split(' ').join('_'),
-                link        = document.createElement('a'),
-                mimeType    = 'application/msword',
-                elHtml      = letter.innerHTML;
+            letterName  = letterName.split(' ').join('_'),
+            link        = document.createElement('a'),
+            mimeType    = 'application/msword',
+            elHtml      = letter.innerHTML;
 
             // 'Click' the generated link to force file download
             link.setAttribute('download', letterName);
@@ -244,8 +251,118 @@ AnimalApp.controller('letterDisplayController', function ($scope, $location, $ro
     // to hide or show the Cause Details
     $scope.update = function(){
         $scope.showDetails = true;
+        $scope.showGuestFields = true;
+        // $scope.causeSelected = $scope.selCause;
+        //
+        // console.log('cause selected: ', $scope.causeSelected);
+
+    }
+    //
+    // STYLE STUFF
+  //   $scope.steps = ['Select Cause', 'Enter Guest Inforamtion', 'View Representatives', 'Preview Letter', 'Print/Save'];
+  // //
+  //   $scope.steps = [
+  //   'Step 1: Team Info',
+  //   'Step 2: Campaign Info',
+  //   'Step 3: Campaign Media'
+  // ];
+
+    $scope.selection = $scope.steps[0];
+    $scope.getCurrentStepIndex = function(){
+        // Get the index of the current step given selection
+        return _.indexOf($scope.steps, $scope.selection);
+    };
+
+    // Go to a defined step index
+    $scope.goToStep = function(index) {
+        if ( !_.isUndefined($scope.steps[index]) )
+        {
+            $scope.selection = $scope.steps[index];
+        }
+    };
+
+    $scope.hasNextStep = function(){
+        var stepIndex = $scope.getCurrentStepIndex();
+        var nextStep = stepIndex + 1;
+        // Return true if there is a next step, false if not
+        return !_.isUndefined($scope.steps[nextStep]);
+    };
+
+    $scope.hasPreviousStep = function(){
+        var stepIndex = $scope.getCurrentStepIndex();
+        var previousStep = stepIndex - 1;
+        // Return true if there is a next step, false if not
+        return !_.isUndefined($scope.steps[previousStep]);
+    };
+
+    $scope.incrementStep = function() {
+        if ( $scope.hasNextStep() )
+        {
+            var stepIndex = $scope.getCurrentStepIndex();
+            var nextStep = stepIndex + 1;
+            $scope.selection = $scope.steps[nextStep];
+        }
+    };
+
+    $scope.decrementStep = function() {
+        if ( $scope.hasPreviousStep() )
+        {
+            var stepIndex = $scope.getCurrentStepIndex();
+            var previousStep = stepIndex - 1;
+            $scope.selection = $scope.steps[previousStep];
+        }
+    };
+
+
+
+
+    // Guest address section
+    $scope.address = {choice: undefined};
+    $scope.loginErrors = '';
+    $scope.regErrors = {
+        firstName       : '',
+        lastName        : '',
+        password        : '',
+        confirmPassword : '',
+        addrNotFound    : '',
+          address       : '',
+          city          : '',
+          state         : '',
+          zip           : ''
+    };
+    var errorMessages = {
+        firstName           : 'First name field is required',
+        lastName            : 'Last name field is required',
+        email               : 'Last name field is required',
+        password            : 'Password is required',
+        confirmPassword     : 'Passwords must match',
+        address             : 'Address field is required',
+        city                : 'City field is required',
+        state               : 'State field is required',
+        zip                 : 'Zip field is required'
     }
 
 
+    $scope.registerAddress = function() {
+        $scope.regErrors.addrNotFound = '';
+
+        if ($scope.addr && $scope.city && $scope.state && $scope.zip) {
+            var address = {
+                address: $scope.addr,
+                city   : $scope.city,
+                state  : $scope.state,
+                zip    : $scope.zip };
+
+            $http.post('/addressConfirmation', address).success(function(data) {
+                if (data == 'Not Found') {
+                    $scope.regErrors.addrNotFound = 'Address is not found, Please double check your address fields';
+                } else {
+                    if (typeof(data) == 'object') {
+                        // Present all the choices and wait for them to pick
+                        $scope.choices = data; }
+                }
+            })
+        }
+    }
 });
-$scope.addr + ', ' + $scope.city + ' ' + $scope.state + ', ' + $scope.zip;
+// $scope.addr + ', ' + $scope.city + ' ' + $scope.state + ', ' + $scope.zip;
